bafe0c66adcc4e83ee68e8c7383e9d4b8a6d5cce
agelastes <erstedy1517@mail.ru>
2019-02-02 21:09:28 +0300

    Update WeatherList.js

--- WeatherList.js
+++ WeatherList.js
@@ -1,24 +1,69 @@
-import React from 'react';
+import React, {Component} from 'react';
+import './Weather.css';
 
 
 
-const WeatherList = (props) => {
 
-    const data = props.data;
-    const weatherList = data.map((data) =>
-        <div>
-            <h2>{data.name}</h2>
-            <h2>{data.temp}</h2>
-        </div>
+const Weather = (props) => {
 
-    );
+    const {name, temp} = this.props;
 
-    return (
+  return(
 
-            <div>{weatherList}</div>
+      <div>
+          <div className="WeatherList-item">
+              <h2>{name}</h2>
+              <h2>{temp + " °C"}</h2>
+          </div>
+          <div>
+              <input type = "Button" value = 'удалить' onClick={props.deleteItem} />
+          </div>
+      </div>
 
     );
 
 };
+
+
+class WeatherList extends Component {
+
+    constructor(props)
+
+    {
+        super(props);
+
+        this.state = {
+          cityDates: props.data
+        };
+    }
+
+
+    deleteCity = (id) => {
+
+        alert(this.state.data);
+
+        this.setState({data: []});
+
+
+    };
+
+
+    render() {
+
+        const weatherList = this.props.data.map((data) =>
+
+            <Weather data={data} key={data.id} deleteItem = {() => this.deleteCity(data.id)} />
+
+        );
+
+        return (
+
+            <div className = "WeatherList">{weatherList}</div>
+
+        );
+    }
+
+}
+
 
 export default WeatherList;
